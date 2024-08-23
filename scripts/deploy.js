0c8182230b9e038ab9daf703b062eb696eb49d97
Matt Hamilton <matt@dharach.com>
2024-08-23 12:05:14 -0400

    Switch to nomicfoundation ethers, rather then old nomiclabs ones

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,25 @@
 async function main() {
-  // Compile contracts
-  await hre.run('compile');
+    // Compile contracts
+    await hre.run('compile');
+    
+    // Deploy the CaptureTheFlag contract
+    const CaptureTheFlag = await hre.ethers.getContractFactory("CaptureTheFlag");
+    const captureTheFlag = await CaptureTheFlag.deploy();
+    await captureTheFlag.waitForDeployment();
+    
+    console.log("CaptureTheFlag deployed to:", await captureTheFlag.getAddress());
 
-  // Deploy the CaptureTheFlag contract
-  const CaptureTheFlag = await hre.ethers.getContractFactory("CaptureTheFlag");
-  const captureTheFlag = await CaptureTheFlag.deploy();
-  await captureTheFlag.deployed();
-
-  console.log("CaptureTheFlag deployed to:", captureTheFlag.address);
-
-  // Deploy MyPlayerContract (which implements PlayerContract)
-  const MyPlayerContract = await hre.ethers.getContractFactory("MyPlayerContract");
-  const myPlayerContract = await MyPlayerContract.deploy();
-  await myPlayerContract.deployed();
-
-  console.log("MyPlayerContract deployed to:", myPlayerContract.address);
+    // Deploy MyPlayerContract (which implements PlayerContract)
+    const MyPlayerContract = await hre.ethers.getContractFactory("MyPlayerContract");
+    const myPlayerContract = await MyPlayerContract.deploy();
+    await myPlayerContract.waitForDeployment();
+    
+    console.log("MyPlayerContract deployed to:", await myPlayerContract.getAddress());
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+	console.error(error);
+	process.exit(1);
+    });
