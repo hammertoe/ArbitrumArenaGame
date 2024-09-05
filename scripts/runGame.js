62885c4faa69c06b46b4239c9a330bcfc632225d
Matt Hamilton <matt@dharach.com>
2024-09-05 17:52:48 -0400

    Iterate through a number of game turns and iterate the players

--- scripts/runGame.js
+++ scripts/runGame.js
@@ -1,6 +1,8 @@
 const hre = require("hardhat");
 require('dotenv').config();
 
+const GAME_TURNS = 10;
+
 async function main() {
      // Get the CaptureTheFlag contract address from the environment variable
     const captureTheFlagAddress = process.env.CAPTURE_THE_FLAG_CONTRACT_ADDRESS;
@@ -11,11 +13,17 @@ async function main() {
     // Attach to the CaptureTheFlag contract
     const CaptureTheFlag = await hre.ethers.getContractFactory("CaptureTheFlag");
     const captureTheFlag = await CaptureTheFlag.attach(captureTheFlagAddress);
+    const numPlayers = await captureTheFlag.getNumPlayers();
 
-     // Print out the location of each player
-     for (let i = 0; i < NUMBER_OF_PLAYERS; i++) {
-        const player = await captureTheFlag.players(i);
-        console.log(`Player ${i + 1} location: x=${player.x}, y=${player.y}`);
+    for (let i = 0; i < GAME_TURNS; i++) {
+        console.log(`Game turn ${i + 1}`);
+        // iterate players
+        await captureTheFlag.iteratePlayers();
+        // Print out the location of each player
+        for (let i = 0; i < numPlayers; i++) {
+            const player = await captureTheFlag.players(i);
+            console.log(`Player ${i + 1} location: x=${player.x}, y=${player.y}, score=${player.score}`);
+        }
     }
 }
 
